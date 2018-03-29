3219146b679bd433d97ab5460060d7a8567b2b0b
Dave Brophy <dave@brophy.uk>
2018-03-29 17:40:10 +0200

    Create writer.go

---
+++ writer.go
@@ -0,0 +1,27 @@
+package main
+
+import "github.com/gopherjs/gopherjs/js"
+
+type Writer struct {
+	Top bool
+	pre *js.Object
+}
+
+func (w *Writer) Write(b []byte) (int, error) {
+	if w.pre == nil {
+		doc := js.Global.Get("document")
+		if doc.Call("getElementsByTagName", "pre").Length() > 0 {
+			w.pre = doc.Call("getElementsByTagName", "pre").Index(0)
+		} else {
+			w.pre = doc.Call("createElement", "pre")
+			body := doc.Call("getElementsByTagName", "body").Index(0)
+			body.Call("appendChild", w.pre)
+		}
+	}
+	if w.Top {
+		w.pre.Set("innerHTML", string(b)+w.pre.Get("innerHTML").String())
+	} else {
+		w.pre.Set("innerHTML", w.pre.Get("innerHTML").String()+string(b))
+	}
+	return len(b), nil
+}
